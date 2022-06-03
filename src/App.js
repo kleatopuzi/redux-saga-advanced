2807fc219499757e9bccc78ac2147509143b838a
kleatopuzi <ktopuzii@gmail.com>
2022-06-03 14:53:58 +0200

    refactior with useReducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import UseReducerExample from "./components/use-reducer-example/use-reducer.component";
 import SagasExample from "./components/sagas-example/sagas-example.component";
 
 import "./App.css";
@@ -7,7 +7,8 @@ import "./App.css";
 const App = (props) => {
   return (
     <div className="App">
-      <SagasExample />
+      <UseReducerExample />
+      {/* <SagasExample /> */}
     </div>
   );
 };
